3c13b4fb89d7f8b43b509099afb7e2e1a2048645
simen wiik <simenwiik@hotmail.com>
2021-09-03 22:48:24 +0200

    Adds the "register user" screen and fixes elements hidden by virtual keyboard

--- app/src/main/java/ntnu/simenwii/fant/MainActivity.kt
+++ app/src/main/java/ntnu/simenwii/fant/MainActivity.kt
@@ -1,28 +1,32 @@
 package ntnu.simenwii.fant
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import ntnu.simenwii.fant.ui.theme.FantTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // https://stackoverflow.com/a/64053897
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             FantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(
-                        Modifier.fillMaxSize()
-                    ) {
-                        Navigation()
-                        BottomBar()
-                    }
+                Scaffold(
+                    bottomBar = { BottomBar() }
+                ) {
+                    Navigation()
                 }
             }
         }
--- app/src/main/java/ntnu/simenwii/fant/screen/NewUser.kt
+++ app/src/main/java/ntnu/simenwii/fant/screen/NewUser.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -15,6 +16,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -22,31 +25,126 @@ import androidx.navigation.NavController
 
 @Composable
 fun NewUser(username: String? = "", password: String? = "", navController: NavController) {
-    Column(
-        Modifier.fillMaxSize().verticalScroll(ScrollState(0)),
-        verticalArrangement = Arrangement.spacedBy(15.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val firstname = remember { mutableStateOf(TextFieldValue("")) }
-        val lastname = remember { mutableStateOf(TextFieldValue("")) }
-        val streetAddress = remember { mutableStateOf(TextFieldValue("")) }
-        val city = remember { mutableStateOf(TextFieldValue("")) }
-        val postalCode = remember { mutableStateOf(TextFieldValue("")) }
-        val email = remember { mutableStateOf(TextFieldValue("")) }
-        val username = remember { mutableStateOf(TextFieldValue(username!!)) }
-        val password = remember { mutableStateOf(TextFieldValue(password!!)) }
+Column(
+    modifier = Modifier.fillMaxSize().verticalScroll(ScrollState(0)),
+    verticalArrangement = Arrangement.spacedBy(15.dp),
+    horizontalAlignment = Alignment.CenterHorizontally
+) {
+    val firstname = remember { mutableStateOf(TextFieldValue("")) }
+    val firstnameFocus = remember { FocusRequester() }
 
-        TextField(firstname.value, label = { Text("First Name") }, onValueChange = { firstname.value = it })
-        TextField(lastname.value, label = { Text("Last Name") }, onValueChange = { lastname.value = it })
-        TextField(streetAddress.value, label = { Text("Street address") }, onValueChange = { streetAddress.value = it })
-        TextField(city.value, label = { Text("City") }, onValueChange = { city.value = it })
-        TextField(postalCode.value, label = { Text("Postal code") }, onValueChange = { postalCode.value = it })
-        TextField(email.value, label = { Text("Email address") }, onValueChange = { email.value = it })
-        TextField(username.value, label = { Text("Username") }, onValueChange = { username.value = it })
-        TextField(password.value, label = { Text("Password") }, visualTransformation = PasswordVisualTransformation(), onValueChange = { password.value = it })
+    val lastname = remember { mutableStateOf(TextFieldValue("")) }
+    val lastnameFocus = remember { FocusRequester() }
 
-        OutlinedButton({ navController.navigate("") }, Modifier.padding(all = 4.dp)) {
-            Text("Register User")
+    val streetAddress = remember { mutableStateOf(TextFieldValue("")) }
+    val streetAddressFocus = remember { FocusRequester() }
+
+    val city = remember { mutableStateOf(TextFieldValue("")) }
+    val cityFocus = remember { FocusRequester() }
+
+    val postalCode = remember { mutableStateOf(TextFieldValue("")) }
+    val postalCodeFocus = remember { FocusRequester() }
+
+    val email = remember { mutableStateOf(TextFieldValue("")) }
+    val emailFocus = remember { FocusRequester() }
+
+    val username = remember { mutableStateOf(TextFieldValue(username!!)) }
+    val usernameFocus = remember { FocusRequester() }
+
+    val password = remember { mutableStateOf(TextFieldValue(password!!)) }
+    val passwordFocus = remember { FocusRequester() }
+
+    TextField(firstname.value, isError = true, label = { Text("First Name") },
+        modifier = Modifier.focusRequester(firstnameFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> lastnameFocus.requestFocus()
+                else -> {
+                    firstname.value = it
+                }
+            }
         }
+    )
+    TextField(lastname.value, label = { Text("Last Name") },
+        modifier = Modifier.focusRequester(lastnameFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> streetAddressFocus.requestFocus()
+                else -> {
+                    lastname.value = it
+                }
+            }
+        }
+    )
+    TextField(streetAddress.value, label = { Text("Street address") },
+        modifier = Modifier.focusRequester(streetAddressFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> cityFocus.requestFocus()
+                else -> {
+                    streetAddress.value = it
+                }
+            }
+        }
+    )
+    TextField(city.value, label = { Text("City") },
+        modifier = Modifier.focusRequester(cityFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> postalCodeFocus.requestFocus()
+                else -> {
+                    city.value = it
+                }
+            }
+        }
+    )
+    TextField(postalCode.value, label = { Text("Postal code") },
+        modifier = Modifier.focusRequester(postalCodeFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> emailFocus.requestFocus()
+                else -> {
+                    postalCode.value = it
+                }
+            }
+        }
+    )
+    TextField(email.value, label = { Text("Email address") },
+        modifier = Modifier.focusRequester(emailFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> usernameFocus.requestFocus()
+                else -> {
+                    email.value = it
+                }
+            }
+        }
+    )
+    TextField(username.value, label = { Text("Username") },
+        modifier = Modifier.focusRequester(usernameFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> passwordFocus.requestFocus()
+                else -> {
+                    username.value = it
+                }
+            }
+        }
+    )
+    TextField(password.value, label = { Text("Password") }, visualTransformation = PasswordVisualTransformation(),
+        modifier = Modifier.focusRequester(passwordFocus),
+        onValueChange = {
+            when {
+                it.text.contains('\n') -> passwordFocus.freeFocus()
+                else -> {
+                    password.value = it
+                }
+            }
+        }
+    )
+
+    OutlinedButton({ navController.navigate("") }, Modifier.padding(all = 4.dp)) {
+        Text("Register User")
     }
+}
 }
